ab48949d5229e6b0a0a3e0ffb8d86cdd84347b02
fandriyaninkov <fandriyaninkov@gmail.com>
2026-01-19 21:34:25 +0300

    Identity. Добавление User в БД

--- Services/IdentityService/DB/ApplicationDbContext.cs
+++ Services/IdentityService/DB/ApplicationDbContext.cs
@@ -8,4 +8,13 @@ public class ApplicationDbContext : DbContext
     {
 
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        var user = modelBuilder.Entity<User>();
+        user.ToTable("Users");
+        user.HasKey(p => p.Id);
+    }
 }
